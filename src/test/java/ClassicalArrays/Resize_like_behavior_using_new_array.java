83c34deabd11ebfcc97f41c2b72c508174fc0080
Ravichandran Thiyagarajan <guy1981.nice@gmail.com>
2026-04-24 23:30:15 +0530

    few more programs

--- src/test/java/ClassicalArrays/Resize_like_behavior_using_new_array.java
+++ src/test/java/ClassicalArrays/Resize_like_behavior_using_new_array.java
@@ -1,4 +1,22 @@
 package ClassicalArrays;
 
+import java.util.Arrays;
+
 public class Resize_like_behavior_using_new_array {
+
+    static void main() {
+
+        int[] arr = {10,20,30};
+        int newValue = 40;
+        int[] newArry = new int[arr.length + 1];
+
+        for(int i = 0; i < arr.length; i++) {
+            newArry[i] = arr[i];
+        }
+
+        newArry[newArry.length - 1] = newValue;
+
+        System.out.println(Arrays.toString(newArry));
+
+    }
 }
